5d386620af3d0df0257c45871db4fb5440664872
Gleb <jimgearalt@gmail.com>
2025-05-20 21:51:54 +0300

    2

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,8 @@
-class StringBuilder {
-  #value;
+const inputEl = document.getElementById("name-input");
+const outputEl = document.getElementById("name-output");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+inputEl.addEventListener("input", () => {
+  const trimmedValue = inputEl.value.trim();
 
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value = this.#value + str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
